e415afe5ab5df26aab817405c2dda042e02ed5e6
mukesh761 <mukesh737462@gmail.com>
2025-03-11 15:39:09 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,4 +1,4 @@
 import jwt from "jsonwebtoken";
 export const generateToken=(user)=>{
-   return jwt.sign({email:user.email,id:user._id},process.env.JWT_TOKEN);
+   return jwt.sign({email:user.email,id:user._id},process.env.JWT_TOKEN, { expiresIn: '24h' });
 }
